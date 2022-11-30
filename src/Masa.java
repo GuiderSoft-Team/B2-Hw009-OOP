c5072a9dd375d3330b319145ffa545acc147c92d
engin hereser <enginhereser@gmail.com>
2022-11-30 13:07:48 +0100

    Engin ödev9 tamam

---
+++ src/Masa.java
@@ -0,0 +1,59 @@
+public class Masa extends Mobilya implements Dortgen{
+
+    private double genislik;
+    private double derinlik;
+
+    public Masa() {
+    }
+
+    public Masa(String aciklama, double yukseklik, boolean kapaliMi, double genislik, double derinlik) {
+        super(aciklama, yukseklik, kapaliMi);
+        this.genislik = genislik;
+        this.derinlik = derinlik;
+    }
+
+    @Override
+    public void setGenislik(double genislik) {
+        if (genislik<0) this.genislik=0;
+        else this.genislik=genislik;
+
+    }
+
+    @Override
+    public double getGenislik() {
+        return 0;
+    }
+
+    @Override
+    public void setDerinlik(double derinlik) {
+        if (derinlik<0)this.derinlik=0;
+        else this.derinlik=derinlik;
+
+    }
+
+    @Override
+    public double getDerinlik() {
+        return 0;
+    }
+
+    @Override
+    public double alanHesapla() {
+        if (!isKapaliMi()){
+          return genislik*derinlik;
+        }else {
+           return 6*genislik*derinlik;
+        }
+
+    }
+
+    @Override
+    public double hacimHesapla() {
+        return genislik * derinlik * getYukseklik();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"[ genislik : "+genislik+" derinlik :"+derinlik+
+                " yükseklik :"+getYukseklik()+"]";
+    }
+}
