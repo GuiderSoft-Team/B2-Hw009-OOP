35c6569fccec7c44c15332464db86afcce461104
user <onikaankaymaz@gmail.com>
2022-12-01 19:01:20 +0300

    Kaan Kaymaz - 01.12.2022 - Homework 9

---
+++ src/YuvarlakSehpa.java
@@ -0,0 +1,40 @@
+public class YuvarlakSehpa extends Mobilya implements Daire {
+    private double yaricap;
+
+    public YuvarlakSehpa() {
+    }
+
+    public YuvarlakSehpa(String aciklama, double yaricap, double yukseklik, boolean kapaliMi) {
+        super(aciklama, yukseklik, kapaliMi);
+        this.yaricap = yaricap;
+    }
+
+    @Override
+    public void setYaricap(double yaricap) {
+
+    }
+
+    @Override
+    public double getYaricap() {
+        return 0;
+    }
+
+    @Override
+    public double alanHesapla() {
+        if (!isKapaliMi()) {
+            return Math.PI * yaricap * yaricap;
+        } else {
+            return (2 * Math.PI * yaricap * yaricap) + (2 * Math.PI * yaricap * getYukseklik());
+        }
+    }
+
+    @Override
+    public double hacimHesapla() {
+        return Math.PI * yaricap * yaricap * getYukseklik();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[" + yaricap + "x" + getYukseklik() + "]";
+    }
+}
