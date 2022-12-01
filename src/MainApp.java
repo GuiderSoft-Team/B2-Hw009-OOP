722e7416780a4c79a1f9896a054aca33a3f6237f
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-12-01 19:41:23 +0300

    Erkan Cihan YILMAZ has Completed. Dec. 01.2022

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,16 @@
 public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
+        Mobilya mb1=new Masa("Ahşap Masa",70,false,120,60);
+        System.out.println(mb1);
+        System.out.println(((Masa)mb1).alanHesapla());
+        System.out.println(((Masa)mb1).hacimHesapla());
+        Daire daire1=new YuvarlakSehpa("Beyaz Sehpa",40,false,40);
+        System.out.println(daire1);
+        System.out.println(daire1.alanHesapla());
+        System.out.println(daire1.hacimHesapla());
+
+
 
     }
 }
