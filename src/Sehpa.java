8347b30b2659e2a129856cd4699910b6a6fc3388
suleymandurmaz <sdurmaz77@hotmail.com>
2022-12-01 17:18:09 +0300

    SULEYMAN HW009  YAPILDI

---
+++ src/Sehpa.java
@@ -0,0 +1,51 @@
+public class Sehpa extends Mobilya implements Dortgen {
+    private double derinlik;
+    public Sehpa(){
+
+    }
+
+    public Sehpa(String aciklama, double yukseklik, boolean kapaliMİ, double derinlik) {
+        super(aciklama, yukseklik, kapaliMİ);
+        this.derinlik = derinlik;
+    }
+
+    public Sehpa(double derinlik) {
+        this.derinlik = derinlik;
+    }
+
+    @Override
+    public double getGenislik() {
+        return 0;
+    }
+
+    @Override
+    public void setGenislik(double genislik) {
+
+    }
+
+    @Override
+    public void setDerinlik(double derinlik) {
+
+    }
+
+    @Override
+    public double getDerinlik() {
+        return getDerinlik();
+    }
+
+    @Override
+    public double alanHesapla() {
+        return getGenislik()*derinlik;
+    }
+
+    @Override
+    public double hacimHesapla() {
+        return getYukseklik()*derinlik*getGenislik();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%5.2f %5.2f %5.2f",getDerinlik()*getYukseklik()*getGenislik());
+    }
+}
+
---
+++ src/YuvarlakMasa.java
@@ -0,0 +1,45 @@
+public class YuvarlakMasa extends Mobilya implements Daire {
+
+    double yaricap;
+
+    public YuvarlakMasa() {
+
+    }
+
+    public YuvarlakMasa(String aciklama, double yukseklik, boolean kapaliMİ, double yaricap) {
+        super(aciklama, yukseklik, kapaliMİ);
+        this.yaricap = yaricap;
+    }
+
+    public YuvarlakMasa(double yaricap) {
+        this.yaricap = yaricap;
+    }
+
+    @Override
+    public void setYaricap(double yaricap) {
+
+    }
+
+    @Override
+    public double getYaricap() {
+        return getYaricap();
+    }
+
+    @Override
+    public double alanHesapla() {
+
+        return Math.PI*Math.pow(yaricap,2);
+    }
+
+    @Override
+    public double hacimHesapla() {
+
+        return Math.PI*Math.pow(yaricap,2)*getYukseklik();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%5.2f %5.2f ", yaricap, getYukseklik());
+    }
+}
+
