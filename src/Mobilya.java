d9fa82a293ad9ded12c50bb20e804336d474ef97
isakrc52 <114700918+isakrc52@users.noreply.github.com>
2022-12-02 14:33:33 +0300

    isa-hmw 9 has done

---
+++ src/Mobilya.java
@@ -0,0 +1,52 @@
+public class Mobilya {
+
+    private String aciklama;
+    private double yukseklik;
+    private boolean kapaliMi;
+
+    public Mobilya() {
+    }
+
+    public Mobilya(String aciklama, double yukseklik, boolean kapaliMi) {
+        setAciklama(aciklama);
+        setYukseklik(yukseklik);
+        setKapaliMi(kapaliMi);
+    }
+
+    public String getAciklama() {
+        return aciklama;
+    }
+
+    public void setAciklama(String aciklama) {
+        this.aciklama = aciklama;
+    }
+
+    public double getYukseklik() {
+        return yukseklik;
+    }
+
+    public double setYukseklik(double yukseklik) {
+
+        if (yukseklik<0){
+            System.err.println("yükseklik 1 den küçük bir değer alamaz");
+            this.yukseklik =0;
+        }else {
+            this.yukseklik=yukseklik;
+        }
+        return this.yukseklik;
+
+    }
+
+    public boolean isKapaliMi() {
+        return kapaliMi;
+    }
+
+    public void setKapaliMi(boolean kapaliMi) {
+        this.kapaliMi = kapaliMi;
+    }
+
+    @Override
+    public String toString() {
+        return aciklama;
+    }
+}
---
+++ src/Sehpa.java
@@ -0,0 +1,52 @@
+public class Sehpa extends Mobilya implements Dortgen{
+
+    private double genislik;
+    private double derinlik;
+
+    public Sehpa() {
+    }
+
+    public Sehpa(String aciklama, double genislik, double derinlik, double yukseklik, boolean kapaliMi) {
+        super(aciklama, yukseklik, kapaliMi);
+        setGenislik(genislik);
+        setDerinlik(derinlik);
+    }
+
+    @Override
+    public void setGenislik(double genislik) {
+
+    }
+
+    @Override
+    public double getGenislik() {
+        return 0;
+    }
+
+    @Override
+    public void setDerinlik(double derinlik) {
+
+    }
+
+    @Override
+    public double getDerinlik() {
+        return 0;
+    }
+
+    @Override
+    public double alanHesapla() {
+        if (!isKapaliMi()){
+            return genislik*derinlik;
+        }else{
+            return 6*derinlik*genislik;
+        }
+    }
+    @Override
+    public double hacimHesapla() {
+        return genislik*derinlik*getYukseklik();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"["+genislik+"x"+derinlik+"x"+getYukseklik()+"]";
+    }
+}
