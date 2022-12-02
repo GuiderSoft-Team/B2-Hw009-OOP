cd96f2da357c5afbe554021d872ca3d674bd13f9
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2022-12-02 11:42:16 +0300

    2.12.2022

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,31 @@ public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
 
+        Mobilya mobilya1=new Masa("Ahşap Masa",120,60,70,false);
+        System.out.println(mobilya1);
+        System.out.printf("MASA 1 ALANI:   %5.2f\n",((Masa) mobilya1).alanHesapla());
+        System.out.printf("MASA 1 HACMİ: %5.2f\n",((Masa) mobilya1).hacimHesapla());
+        System.out.println();
+
+
+
+
+        Daire daire1=new YuvarlakSehpa("Beyaz sehpa",40,40,false);
+        System.out.println(daire1);
+        System.out.printf("SEHAPANIN ALANI: %5.2f\n",daire1.alanHesapla());
+        System.out.printf("SEHPANIN HACMİ: %5.2f\n",daire1.hacimHesapla());
+
+
+        // YuvarlakSehpa Daire sınıfından implemente edildiği için gerekli değil
+
+
+        /*
+         Dortgen[] mobilyalar=new Dortgen[]{new Sehpa(),new Masa(), new YuvarlakSehpa()};
+        Çalışabilir (DIKDORTGEN) new YuvarlakSehapa şeklinde ama doğru olmaz , yuvarlakSEhpa
+        implements Daire sınıfından
+
+
+         */
+
     }
 }
---
+++ src/Sehpa.java
@@ -0,0 +1,65 @@
+
+public class Sehpa extends Mobilya implements Dortgen{
+
+    private double genislik;
+    private double derinlik;
+
+    public Sehpa() {
+    }
+
+    public Sehpa(String aciklama, double genislik, double derinlik, double yukseklik, boolean kapaliMi) {
+        super(aciklama, yukseklik, kapaliMi);
+        this.genislik = genislik;
+        this.derinlik = derinlik;
+    }
+
+    @Override
+    public int kapaliMi() {
+        return 0;
+    }
+
+    @Override
+    public void setGenislik(double genislik) {
+
+    }
+
+    @Override
+    public double getGenislik() {
+        return 0;
+    }
+
+    @Override
+    public void setDerinlik(double derinlik) {
+
+    }
+
+    @Override
+    public double getDerinlik() {
+        return 0;
+    }
+
+    @Override
+    public double alanHesapla() {
+        if (!isKapaliMi()){
+            return genislik*derinlik;
+        }else {
+            return 6*genislik*derinlik;
+        }
+    }
+
+    @Override
+    public double hacimHesapla() {
+        return genislik*derinlik*getYukseklik();
+    }
+
+     @Override
+
+    public String toString() {
+          return String.format("%s,%d*%d*%d","Ahşap yuavrlak sehpa",getYukseklik()*getGenislik()*getDerinlik());
+
+
+    }
+
+
+}
+
