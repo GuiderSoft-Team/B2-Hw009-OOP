d6e8289b673777a31d947921657304a7aa1bb0a8
Gürhan Ustali <gurhanustali@gmail.com>
2022-12-05 21:35:07 +0100

    Homework 9 has been solved.

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,16 @@ public class MainApp {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz.
 
+        Mobilya mobilya1=new Masa("Ahşap Masa",120,false,60,70);
+        System.out.println(mobilya1);
+        System.out.println("Masanın Alanı : "+((Masa)mobilya1).alanHesapla());
+        System.out.println("Masanın Hacmi : "+((Masa)mobilya1).hacimHesapla());
+
+        Daire daire1=new YuvarlakSehpa("Beyaz Sehpa",40,false,40);
+        System.out.println(daire1);
+        System.out.printf("Sehpanın alanı : %5.2f\n",daire1.alanHesapla());
+        System.out.printf("Sehpanın hacmi : %5.2f\n",daire1.hacimHesapla());
+
+        //Dortgen[] mobilyalar=new Dortgen[]{new Sehpa(),new Masa(),new YuvarlakSehpa()};
     }
 }
